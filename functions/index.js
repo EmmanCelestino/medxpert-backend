d75222961093be638abafe45c1158c760f0f8cf9
Emmanuel Celestino <emmanuel.celestino93@gmail.com>
2023-05-06 16:27:48 +0800

    Added email sending for appointment creation

--- functions/index.js
+++ functions/index.js
@@ -84,3 +84,85 @@ exports.onUserCreate = functions.firestore.document('users/{userId}').onCreate( 
   }
 
 })
+
+exports.onAppointmentCreate = functions.firestore.document('appointments/{appointmentId}').onCreate( async (snap , context) => {
+
+  let newAppointment = snap.data()
+
+  const smtpTransport = mailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: functions.config().gmail.email_address,
+      pass: functions.config().gmail.passkey
+    }
+  })
+
+  let patient = await admin.firestore().collection('patients').doc(newAppointment.patientUid).get()
+  let clinic = await admin.firestore().collection('clinics').doc(newAppointment.clinicUid).get()
+  let doctor = await admin.firestore().collection('doctors').doc(newAppointment.doctorUid).get()
+
+  let user
+
+  if(patient.data()){
+    user = await admin.firestore().collection('users').doc(patient.data().userUid).get()
+  }
+
+  if(user){
+
+    const emailAddress = user.data().emailAddress;
+
+    const emailContent = `
+      <html>
+        <body>
+            <div style="width: 100%; font-family: Roboto;" class="content">
+              <div>
+                <h2 style="text-align: center; font-weight: 700; color : #3E6680; ">Appointment Details</h2>
+              </div>
+              <div style="margin-top: 50px; color: black;">
+                <p>Hi ${user.data().firstName},</p>
+                <br/>
+                <p>Thank you for booking with ${clinic.data().name} - Dr. ${doctor.data().firstName} ${doctor.data().lastName}. Your appointment is confirmed for ${moment.unix(newAppointment.appointmentDate.seconds).format("dddd, LL")}. If you are unable to make this appointment or would like to change your appointment to a different date, please call ${clinic.data().contactNumber} or email us at ${clinic.data().emailAddress}.</p>
+                <p>Thank you for trusting medXpert.</p>
+                <br/>
+                <br/>
+                <p>Yours truly,</p>
+                <p>Our medXpert team</p>
+              </div>
+            </div>
+        </body>
+      </html>
+    `
+
+    const mail = {
+      from: functions.config().gmail.email_address,
+      to: emailAddress,
+      subject: "Welcome to medXpert",
+      text: emailContent,
+      html: emailContent
+    }
+    
+    smtpTransport.sendMail(mail, (error, response) => {
+
+      let code = 200
+      let message = "Email sent successfully!"
+
+      if (error) {
+        console.error(error);
+        console.error("[ERROR] Email not sent.")
+        code = 500
+        message = "Email not sent. Something went wrong"
+        
+      } else {
+        console.info("[SUCCESS] Welcome email sent to "+emailAddress)
+      }
+
+      smtpTransport.close();
+      return { code : code, message : message }
+      
+    })
+
+  }
+
+  
+
+})
